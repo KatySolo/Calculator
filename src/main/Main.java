0919e361579542a497d232b14e2f0e9a95688e87
Solodovnikova Ekaterina <colo18@yandex.ru>
2017-11-26 17:48:22 +0500

    final working version of regular calc

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,44 +13,38 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Lexer lexer = new Lexer();
-
-        lexer.register(new ComplexNumberReader());
-        lexer.register(new VectorReader());
-        lexer.register(new IntReader());
-        lexer.register(new OperationReader());
-        lexer.register(new WhitespaceReader());
-        lexer.register(new WordReader());
-        lexer.register(new VectorReader());
-        lexer.register(new DoubleReader());
-
-        try {
-            System.out.println(lexer.tokenize("2i+1-2i"));
-            System.out.println(lexer.tokenize("(1,2,3) + (2,4,6)"));
-            System.out.println(lexer.tokenize("String a = 'ccc'"));
-            System.out.println(lexer.tokenize("24 + 53 / 2"));
-            System.out.println(lexer.tokenize("2.4+4.5"));
-
-//            Pattern pattern = Pattern.compile("[+-]?([0-9]*[.])?[0-9]+");
-//            Matcher m = pattern.matcher("2.4 + 3.5+ 6.8");
+//        Lexer lexer = new Lexer();
 //
-//            while(m.find())
-//                System.out.println(m.group());
+//        lexer.register(new ComplexNumberReader());
+//        lexer.register(new VectorReader());
+//        lexer.register(new IntReader());
+//        lexer.register(new OperationReader());
+//        lexer.register(new WhitespaceReader());
+//        lexer.register(new WordReader());
+//        lexer.register(new VectorReader());
+//        lexer.register(new DoubleReader());
 
-        } catch (Exception e) {
-            System.out.print(e.getMessage());
-        }
-
-//        Calculator calc = new Calculator();
-////        System.out.println(calc.GetExpression("56*(2+3*4)/3"));
 //        try {
-//            System.out.println(calc.Calculate("2+3*4")); //14
-//            System.out.println(calc.Calculate("(23+4)*2")); // 27*2 = 54
-//            System.out.println(calc.Calculate("3^2 + 45 ")); //9+45 =  54
+//            System.out.println(lexer.tokenize("2i+1-2i"));
+//            System.out.println(lexer.tokenize("(1,2,3) + (2,4,6)"));
+//            System.out.println(lexer.tokenize("String a = 'ccc'"));
+//            System.out.println(lexer.tokenize("24 + 53 / 2"));
+//            System.out.println(lexer.tokenize("2.4+4.5"));
+//
 //        }
-//        catch (Exception e){
-//            System.out.println(e.getMessage());
+//        } catch (Exception e) {
+//            System.out.print(e.getMessage());
 //        }
+
+        Calculator calc = new Calculator();
+        try {
+            System.out.println(calc.Calculate("2+3*4")); //14
+            System.out.println(calc.Calculate("(23+4)*2")); // 27*2 = 54
+            System.out.println(calc.Calculate("3^2 + 45 ")); //9+45 =  54
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
